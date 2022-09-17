fa7c6910ff165ad1a222c95c5ccba3b4c76a39fd
official-imsny <fatehsingh.sunny5@gmail.com>
2022-09-17 22:56:21 +0530

    variable: set default value and read mutiple variables

---
+++ practice/06-hello-variable-default/hello-variable.tf
@@ -0,0 +1,9 @@
+# terraform plan --> attractive mode
+# terraform plan -var username="Fateh Singh" -var age="29" --> Non attractive mode
+
+output "printusername" {
+  value = "Hello, ${var.username}, your age is ${var.age}"
+}
+
+
+
