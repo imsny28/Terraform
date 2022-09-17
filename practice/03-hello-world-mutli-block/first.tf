5dd9d337a2824bbdf8dd931d2b1ec2dfa5447b2a
official-imsny <fatehsingh.sunny5@gmail.com>
2022-09-17 21:33:55 +0530

    hello world muti block in same file

---
+++ practice/03-hello-world-mutli-block/first.tf
@@ -0,0 +1,12 @@
+
+output "firstoutblock" {
+  value = "This is my first output block"
+}
+
+output "secondoutblock" {
+  value = "This is my second output block"
+}
+
+output "thirdoutblock" {
+  value = "This is my third output block"
+}
