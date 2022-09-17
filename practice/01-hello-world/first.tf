4efe868bfee863a93d5b793bdb74b5a898e1f595
official-imsny <fatehsingh.sunny5@gmail.com>
2022-09-17 21:23:17 +0530

    Hello World output block

---
+++ practice/01-hello-world/first.tf
@@ -0,0 +1,15 @@
+# This is comment line
+// This is also a comment line
+
+/*
+	Syntax
+	block "label1" label2 {
+		identifier = expression
+	}
+*/
+
+# As of now use terraform plan
+
+output "hello" {
+  value = "Hello World"
+}
