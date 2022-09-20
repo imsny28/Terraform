0a003d4f8c1dee23325bd708630852e4a1f720f8
official-imsny <fatehsingh.sunny5@gmail.com>
2022-09-20 08:16:44 +0530

    AWS Data source

---
+++ practice/18-aws-datasource/provider.tf
@@ -0,0 +1,38 @@
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "ami_id" {
+  value = data.aws_ami.ubuntu.id
+}
