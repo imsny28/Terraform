67568e2bb78cef1abf8989736589e1ba510ee957
official-imsny <fatehsingh.sunny5@gmail.com>
2022-09-20 08:49:18 +0530

    Terraform Plugin - git module

--- practice/15-terraform-first-resource/terraform.tf
+++ practice/15-terraform-first-resource/terraform.tf
@@ -25,3 +25,7 @@
 
 # The terraform taint command manually marks a Terraform-managed resource as tainted, forcing it to be destroyed and recreated on the next apply.
 # terraform taint aws_security_group.allow_tls
+
+# The output of terraform graph is in the DOT format, which can easily be converted to an image by making use of dot provided by GraphViz:
+# terraform graph | dot -Tsvg > graph.svg
+# terraform graph | dot -Tpdf > graph.pdf
